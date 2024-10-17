cd467775ea314f028fc784ffb22778e2f91ddca2
pablitto8 <swierczynski1i2021@gmail.com>
2024-10-17 14:17:31 +0200

    lekcja php 17.10.2024
    tablice

---
+++ PawelSwierczynski17.php
@@ -0,0 +1,175 @@
+<?php 
+
+
+// $tablica = [];
+
+// for ($i = 0; $i < 10; $i++) {
+//     $tablica[$i] = rand(0, 10);
+// }
+
+// print_r($tablica);
+
+// for ($i=0; $i < count($tablica); $i++) { 
+//     if ($tablica[$i] % 2 == 0) {
+//        print_r( "parzysta-" .$tablica[$i] . " ");
+//     }
+//     if ($tablica[$i] % 2 != 0) {
+//         print_r("nieparzysta-" . $tablica[$i] . " ");
+//      }
+// }
+
+
+
+
+
+
+// $tablica = [];
+
+// for ($i = 0; $i < 10; $i++) {
+//     $tablica[$i] = rand(0, 10);
+// }
+
+// print_r($tablica);
+
+// $min = min($tablica);
+// $max = max($tablica);
+
+// echo "Najmniejsza " . $min . "\n";
+// echo "Najwieksza" . $max . "\n";
+
+
+
+
+// $tablica = [];
+
+// for ($i = 0; $i < 10; $i++) {
+//     $tablica[$i] = rand(0, 10);
+// }
+
+// print_r($tablica);
+
+// $suma = array_sum($tablica);
+// $ilosc = count($tablica);
+// $srednia = $suma / $ilosc;
+
+// echo $srednia;
+
+
+
+
+// $tablica = [];
+
+// for ($i = 0; $i < 10; $i++) {
+//     $tablica[$i] = rand(0, 10);
+// }
+
+// print_r($tablica);
+
+// $parzysta = 0
+// $nieparzysta = 0
+
+// for ($i=0; $i < 10; $i++) { 
+//     if ($tablica[$i] % 2 == 0) {
+//         $parzysta = $parzysta + 1;
+//     }
+//     if ($tablica[$i] % 2 != 0) {
+//         $nieparzysta = $nieparzysta - 1;
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $tablica = [];
+
+// for ($i = 0; $i < 100; $i++) {
+//     $tablica[$i] = rand(0, 100);
+// }
+
+
+
+// $wPrzedziale = [];
+
+
+// for ($i=0; $i < count($tablica); $i++) { 
+//     if ($tablica[$i] > 5 && $tablica[$i] < 15) {
+//         $wPrzedziale[] = $tablica[$i];
+//     }
+// }
+
+// echo count($wPrzedziale);
+
+
+
+// $tablica = [];
+
+// for ($i = 0; $i < 100; $i++) {
+//     $tablica[$i] = rand(0, 100);
+// }
+
+
+// $nowatablica = [];
+
+// for ($i=0; $i < 100; $i++) { 
+//     if ($nowatablica) {
+    
+//     }
+// }
+
+
+
+
+// $tablica = [];
+
+// for ($i = 0; $i < 100; $i++) {
+//     $tablica[$i] = rand(0, 100);
+// }
+
+// rsort($tablica);
+// print_r($tablica);
+
+
+
+
+// $tablica = [];
+
+// for ($i = 0; $i < 100; $i++) {
+//     $tablica[$i] = rand(0, 100);
+// }
+
+// $nowatablica = [];
+
+// for ($i=0; $i < 100; $i++) { 
+//     if ($tablica[$i] > 10) {
+//         $nowatablica[] = $tablica[$i];
+//         print_r($nowatablica);
+//     }
+// }
+
+
+
+
+
+
+?>
